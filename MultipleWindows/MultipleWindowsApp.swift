3ada77783b62ec76d8b63a0620c89ff1e1fe25b4
Hung-Chun(Carter) Tsai <carterTsai95@gmail.com>
2023-11-13 10:10:19 -0500

    Initialize the project

---
+++ MultipleWindows/MultipleWindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleWindowsApp.swift
+//  MultipleWindows
+//
+//  Created by Hung-Chun Tsai on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct MultipleWindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
